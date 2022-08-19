a71cd171d2ecd43d92b8427b36a01db38b1bc047
HardBoris <bgaibor03@gmail.com>
2022-08-18 20:14:45 -0400

    varios intentos

--- src/searcher.ts
+++ src/searcher.ts
@@ -1,15 +1,9 @@
-import { Product } from "./product";
-import { bindable } from "aurelia-framework";
+import { customElement, bindable } from "aurelia-framework";
 
+@customElement("searcher")
 export class Searcher {
-  @bindable busca: string;
+  @bindable type = "text";
+  @bindable label: string;
+  @bindable placeholder = "";
+  @bindable value: string;
 }
-
-/* export class prodValueConverter {
-  toView(items: Product[], search: string){
-    if (search) {
-      console.log(search)
-      return items.filter((ele) => ele.name.toLowerCase().includes(search.toLowerCase()))
-    } else return items;
-  }
-} */
