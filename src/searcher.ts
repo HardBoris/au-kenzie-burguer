65c0973e05c82f73b1cd2e24872f08fab02a91d4
HardBoris <bgaibor03@gmail.com>
2022-09-21 10:48:06 -0400

    algunos cambios

--- src/searcher.ts
+++ src/searcher.ts
@@ -1,15 +1,16 @@
 import { Product } from './product';
 import {bindable} from "aurelia-framework"
+import { ProductType } from 'product-service';
 
 export class Searcher {
   @bindable busca: string;
 }
 
-export class prodValueConverter {
-  toView(items: Product[], search: string){
+/* export class prodValueConverter {
+  toView(items: ProductType[], search: string){
     if (search) {
       console.log(search)
       return items.filter((ele) => ele.name.toLowerCase().includes(search.toLowerCase()))
     } else return items;
   }
-}
+} */
